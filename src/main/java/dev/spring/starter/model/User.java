d9212e5ef4ad747c71f358e2607961620c72fe77
VBortnyk <viacheslav.bortnyk@gmail.com>
2020-06-07 10:50:21 +0300

    hw 32 solution

--- src/main/java/dev/spring/starter/model/User.java
+++ src/main/java/dev/spring/starter/model/User.java
@@ -25,6 +25,14 @@ public class User implements Cloneable {
         this.password = password;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
